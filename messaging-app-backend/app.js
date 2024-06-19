27a17bfe03030b04080cf6bafd886b5972d171b1
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-06-19 15:50:26 +0200

    Feat: add routes as object via index
    * Routes are available through the object in app.js

--- messaging-app-backend/app.js
+++ messaging-app-backend/app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
-const indexRouter = require("./routes/index");
+const routes = require("./routes");
 require("./config/database");
 const cors = require("cors");
 const app = express();
@@ -20,7 +20,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
+app.use("/user", routes.users);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- messaging-app-backend/routes/index.js
+++ messaging-app-backend/routes/index.js
@@ -1,9 +1,3 @@
-const express = require("express");
-const router = express.Router();
+const users = require("./users");
 
-/* GET home page. */
-router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
-});
-
-module.exports = router;
+module.exports = { users };
