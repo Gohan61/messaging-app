8ec43779a5b7bf01078c44e2963d7e0b35d5462f
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-06-19 14:01:51 +0200

    Feat: setup cors allow all for development

--- messaging-app-backend/app.js
+++ messaging-app-backend/app.js
@@ -5,7 +5,10 @@ const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const indexRouter = require("./routes/index");
 require("./config/database");
+const cors = require("cors");
 const app = express();
+
+app.use(cors());
 
 // view engine setup
 // app.set('views', path.join(__dirname, 'views'));
