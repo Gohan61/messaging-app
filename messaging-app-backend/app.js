50638628b83f6b0ae02419670c16dcdaae795d23
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-06-19 14:00:10 +0200

    Feat: setup DB connection

--- messaging-app-backend/app.js
+++ messaging-app-backend/app.js
@@ -4,6 +4,7 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const indexRouter = require("./routes/index");
+require("./config/database");
 const app = express();
 
 // view engine setup
