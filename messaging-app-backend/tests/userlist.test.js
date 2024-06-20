6bf5d28bb6235d976b44f45ad23b5905ccf53282
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-06-20 13:51:32 +0200

    Fix: get new token + correct status + user check
    * Instead of getting token from ENV variables, a test login is made
    which returns a new token every time that does not expire and could
    possibly fail test
    * Without token test returns 401 instead of 500
    * Chcking user not to be null, leads to false positive as user obj was
    undefined. Now checking specific property of user obj preventing false
    positive

--- messaging-app-backend/tests/userlist.test.js
+++ messaging-app-backend/tests/userlist.test.js
@@ -3,29 +3,55 @@ const express = require("express");
 const app = express();
 const user = require("../routes/users");
 const initializeMongoServer = require("../config/databaseTest");
+require("../config/passportTest");
 const Usermodel = require("../models/user");
-require("dotenv").config();
+const bcrypt = require("bcryptjs");
+let token;
 
 beforeAll(async () => {
   await initializeMongoServer();
 
-  const users = new Usermodel({
-    first_name: "Test",
-    last_name: "test",
-    username: "testing",
-    password: "testing",
-    age: 12,
-    bio: "I am new here testing things out",
-  });
+  const password = "testing";
+  try {
+    bcrypt.hash(password, 10, async (err, hashedPassword) => {
+      const users = new Usermodel({
+        first_name: "Test",
+        last_name: "test",
+        username: "testing",
+        password: hashedPassword,
+        age: 14,
+        bio: "I am new here testing things out",
+      });
 
-  users.save();
+      await users.save();
+    });
+  } catch (err) {
+    console.log(err);
+  }
 });
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use("/user", user);
+
+test("User can sign in", async () => {
+  const payload = { username: "testing", password: "testing" };
+
+  const res = await request(app)
+    .post("/user/signin")
+    .send(payload)
+    .set("Content-Type", "application/json")
+    .set("Accept", "application/json")
+    .then((res) => {
+      expect(res.body.username).toEqual("testing");
+      expect(res.body.token).not.toBeNull();
+      token = res.body.token;
+    });
+});
 
 test("Returns the user list", async () => {
   const authorization = {
-    Authorization: process.env.testToken,
+    Authorization: `Bearer ${token}`,
   };
 
   const res = await request(app)
@@ -33,7 +59,7 @@ test("Returns the user list", async () => {
     .set("Content-Type", "application/json")
     .set(authorization)
     .then((res) => {
-      expect(res.users).not.toBeNull();
+      expect(res.body.users[0].username).toBe("testing");
     });
 });
 
@@ -42,6 +68,6 @@ test("Returns a server error without authentication", async () => {
     .get("/user/userlist")
     .set("Content-Type", "application/json")
     .then((res) => {
-      expect(res.status).toBe(500);
+      expect(res.status).toBe(401);
     });
 });
