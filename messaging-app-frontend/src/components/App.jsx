733d4989dc11549bc213a6f5ca437491988cdd41
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-06-21 15:15:32 +0200

    Feat: link to signin/signup Navbar + login status
    * Display logout status when logged in + sign in/sign up on logout

--- messaging-app-frontend/src/components/App.jsx
+++ messaging-app-frontend/src/components/App.jsx
@@ -4,10 +4,17 @@ import Navbar from "./Navbar";
 import { Outlet } from "react-router-dom";
 
 function App() {
+  const [loginStatus, setLoginStatus] = useState(() => {
+    if (localStorage.getItem("Token")) {
+      return true;
+    } else {
+      return false;
+    }
+  });
   return (
     <>
-      <Navbar />
-      <Outlet></Outlet>
+      <Navbar props={{ loginStatus, setLoginStatus }} />
+      <Outlet context={[loginStatus, setLoginStatus]}></Outlet>
     </>
   );
 }
--- messaging-app-frontend/src/components/Navbar.jsx
+++ messaging-app-frontend/src/components/Navbar.jsx
@@ -1,12 +1,27 @@
 import { Link } from "react-router-dom";
 
-export default function Navbar() {
+export default function Navbar({ props }) {
   return (
     <>
       <nav>
         <a href="/">Book Stranger Chat</a>
-        <Link to={"signin"}>Sign in</Link>
-        <Link to={"signup"}>Sign up</Link>
+        {props.loginStatus ? (
+          <button
+            onClick={() => {
+              localStorage.removeItem("Token");
+              localStorage.removeItem("username");
+              localStorage.removeItem("userId");
+              props.setLoginStatus(false);
+            }}
+          >
+            Logout
+          </button>
+        ) : (
+          <>
+            <Link to={"signin"}>Sign in</Link>
+            <Link to={"signup"}>Sign up</Link>
+          </>
+        )}
       </nav>
     </>
   );
