fc0f8951ed16d1ecc131dd913d2122924625e64c
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-06-21 15:16:49 +0200

    Feat: render Signin form + fetch to DB for token
    * Token + User details are saved to localStorage for App functionality
    on authorization routes on frontend

---
+++ messaging-app-frontend/src/components/Signin.jsx
@@ -0,0 +1,69 @@
+import { useState } from "react";
+
+export default function Signin() {
+  const [signupForm, setSignupForm] = useState({
+    username: "",
+    password: "",
+  });
+  const [error, setError] = useState("");
+
+  const fetchToken = (event) => {
+    event.preventDefault();
+
+    fetch("http://localhost:3000/user/signin", {
+      mode: "cors",
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: signupForm.username,
+        password: signupForm.password,
+      }),
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((res) => {
+        if (res.token !== undefined) {
+          localStorage.setItem("Token", `Bearer ${res.token}`);
+          localStorage.setItem("userId", res.userId);
+          localStorage.setItem("username", res.username);
+        } else {
+          throw res.error;
+        }
+      })
+      .catch((err) => {
+        setError(err);
+      });
+  };
+
+  return (
+    <>
+      <form action="" method="post">
+        <label htmlFor="username">Username: </label>
+        <input
+          type="text"
+          name="username"
+          id="username"
+          value={signupForm.username}
+          onChange={(e) =>
+            setSignupForm({ ...signupForm, username: e.target.value })
+          }
+        />
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          name="password"
+          id="password"
+          value={signupForm.password}
+          onChange={(e) =>
+            setSignupForm({ ...signupForm, password: e.target.value })
+          }
+        />
+        <button onClick={(e) => fetchToken(e)}>Submit</button>
+      </form>
+      <p className="error">{error}</p>
+    </>
+  );
+}
