df6f1c07b6061c6517e1bb96260b1629ecee31c4
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-06-21 08:12:57 +0200

    Feat: setup react router + routes

--- messaging-app-frontend/src/main.jsx
+++ messaging-app-frontend/src/main.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import Homepage from "./components/Homepage.jsx";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [{ index: true, element: <Homepage /> }],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
-)
+);
