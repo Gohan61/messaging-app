1eda6afac6c30d6c860a9a8b72224842f9709427
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-06-25 18:13:22 +0200

    Refactor: define messages type as mixed in Array
    * In order to assign every message it's content and an id, the Schema
    is changed to allow new data types

--- messaging-app-backend/models/chats.js
+++ messaging-app-backend/models/chats.js
@@ -3,7 +3,9 @@ const Schema = mongoose.Schema;
 
 const ChatSchema = new Schema({
   users: [{ type: Schema.Types.ObjectId, ref: "User", required: true }],
-  messages: { type: Array, required: false, minLength: 1, maxLength: 400 },
+  messages: [
+    { type: Schema.Types.Mixed, required: false, minLength: 1, maxLength: 400 },
+  ],
   date: { type: String, required: true },
 });
 
