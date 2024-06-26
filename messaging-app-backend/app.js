c29b21bd9369ecb8e227e74792b4441d0c122912
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-06-26 16:38:31 +0200

    Feat: allow cors only for hosted frontend

--- messaging-app-backend/app.js
+++ messaging-app-backend/app.js
@@ -9,7 +9,13 @@ require("./config/passport");
 const cors = require("cors");
 const app = express();
 
-app.use(cors());
+const corsOptions = {
+  origin: ["https://messaging-app-odin-frontend.netlify.app"],
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 
 // view engine setup
 // app.set('views', path.join(__dirname, 'views'));
