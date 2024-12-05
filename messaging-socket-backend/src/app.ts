33aff36102ae44c812c94227b474f1388421d3b7
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-12-05 16:09:47 +0100

    Feat: setup Express + Socket.io server
    * Express for handling http requests on webpages
    * Socket.io for handling real time messaging

---
+++ messaging-socket-backend/src/app.ts
@@ -0,0 +1,24 @@
+import express, { NextFunction, Request, Response } from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import "dotenv/config";
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+const httpServer = createServer(app);
+const io = new Server(httpServer);
+
+io.on("connection", (socket) => {
+  socket.emit("connect", { message: "a new client connected" });
+});
+
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  res.status(err.status | 500);
+  res.send({ error: err });
+});
+
+const port = process.env.PORT;
+httpServer.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
