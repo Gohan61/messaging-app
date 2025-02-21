f59341c2a47cf00de840d82d8c99ba7cc92f2d88
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2025-02-21 09:16:47 +0100

    Refactor: type check routes + handle custom errors
    * Errors are now mostly thrown in accordance with the custom error type,
    making them streamlined

--- messaging-socket-backend/src/controllers/signin-upController.ts
+++ messaging-socket-backend/src/controllers/signin-upController.ts
@@ -1,11 +1,17 @@
 import asyncHandler from "express-async-handler";
-import { body, validationResult } from "express-validator";
+import { body, ValidationError, validationResult } from "express-validator";
 import { PrismaClient } from "@prisma/client";
 import bcrypt from "bcryptjs";
 import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 import passport from "passport";
 import jwt from "jsonwebtoken";
 import { User } from "@prisma/client";
+import { Request, Response, NextFunction } from "express";
+import {
+  CustomError,
+  SigninResponse,
+  SingleResponseType,
+} from "../types/types";
 
 const prisma = new PrismaClient();
 
@@ -41,45 +47,51 @@ export const signup = [
     .isLength({ max: 255 })
     .withMessage("Bio can be maximum 255 characters long"),
 
-  asyncHandler(async (req, res, next): Promise<any> => {
-    const errors = validationResult(req);
-    const body: {
-      first_name: string | undefined;
-      last_name: string | undefined;
-      username: string;
-      password: string;
-      bio: string | undefined;
-    } = req.body;
+  asyncHandler(
+    async (
+      req: Request,
+      res: Response<SingleResponseType<string | ValidationError[]>>,
+      next: NextFunction
+    ): Promise<void> => {
+      const errors = validationResult(req);
+      const body: {
+        first_name: string | undefined;
+        last_name: string | undefined;
+        username: string;
+        password: string;
+        bio: string | undefined;
+      } = req.body;
 
-    if (!errors.isEmpty()) {
-      return res.status(500).json({ errors: errors.array() });
-    }
-
-    try {
-      const hashedPassword = bcrypt.hashSync(body.password, 10);
-
-      const user = await prisma.user.create({
-        data: {
-          first_name: body.first_name,
-          last_name: body.last_name,
-          username: body.username,
-          password: hashedPassword,
-          bio: body.bio,
-        },
-      });
-
-      return res.status(200).json({ message: "User saved" });
-    } catch (err) {
-      if (
-        err instanceof PrismaClientKnownRequestError &&
-        err.code === "P2002"
-      ) {
-        return res.status(500).json({ errors: "Username already exists" });
+      if (!errors.isEmpty()) {
+        res.status(500).json({ errors: errors.array() });
       }
 
-      return next(err);
+      try {
+        const hashedPassword = bcrypt.hashSync(body.password, 10);
+
+        const user = await prisma.user.create({
+          data: {
+            first_name: body.first_name,
+            last_name: body.last_name,
+            username: body.username,
+            password: hashedPassword,
+            bio: body.bio,
+          },
+        });
+
+        res.status(200).json({ message: "User saved" });
+      } catch (err) {
+        if (
+          err instanceof PrismaClientKnownRequestError &&
+          err.code === "P2002"
+        ) {
+          throw new CustomError("Username already exists", 500);
+        }
+
+        return next(err);
+      }
     }
-  }),
+  ),
 ];
 
 export const signin = [
@@ -92,52 +104,75 @@ export const signin = [
     .isLength({ min: 1 })
     .withMessage("Password cannot be empty"),
 
-  asyncHandler(async (req, res, next): Promise<any> => {
-    const errors = validationResult(req);
+  asyncHandler(
+    async (
+      req: Request,
+      res: Response<SigninResponse | SingleResponseType<string>>,
+      next: NextFunction
+    ): Promise<void> => {
+      const errors = validationResult(req);
+      let user: User | undefined;
+      let token;
 
-    if (!errors.isEmpty()) {
-      return res
-        .status(404)
-        .json({ message: "Could not login user", errors: errors.array() });
-    }
-
-    passport.authenticate(
-      "local",
-      { session: false },
-      (err: any, user: User, info: any) => {
-        if (err) {
-          return next(err);
-        }
-        if (!user) {
-          res.status(404).json({ errors: "User not found", user });
-        } else {
-          req.logIn(user, { session: false }, (err) => {
-            if (err) {
-              next(err);
-            }
-
-            if (!process.env.SECRET) {
-              throw new Error("Environment variable SECRET is not set");
-            }
-
-            jwt.sign(
-              { user: user },
-              process.env.SECRET,
-              { expiresIn: "10d" },
-              (err, token) => {
-                if (err) {
-                  return next(err);
-                }
-
-                return res.status(200).json({
-                  token: token,
-                  username: user.username,
-                });
-              }
-            );
-          });
-        }
+      if (!errors.isEmpty()) {
+        res
+          .status(404)
+          .json({ message: "Could not login user", errors: errors.array() });
       }
-    )(req, res, next);
-  }),
+
+      try {
+        user = await new Promise<User>((resolve, reject) => {
+          passport.authenticate(
+            "local",
+            { session: false },
+            (err: unknown, user: User, info: any) => {
+              if (err) {
+                return reject(err);
+              }
+              if (!user) {
+                throw new CustomError("User not found", 404);
+              }
+
+              req.logIn(user, { session: false }, (err) => {
+                if (err) {
+                  return reject(err);
+                }
+                resolve(user);
+              });
+            }
+          )(req, res, next);
+        });
+      } catch (err) {
+        next(err);
+      }
+
+      try {
+        if (!user) {
+          throw new CustomError("Something went wrong", 500);
+        }
+
+        jwt.sign(
+          { user: user },
+          process.env.SECRET as string,
+          { expiresIn: "10d" },
+          (err, token) => {
+            if (err) {
+              return next(err);
+            }
+
+            if (!token) {
+              return next(new CustomError("JWT could not be generated", 500));
+            }
+
+            return res.status(200).json({
+              token: token,
+              username: user.username,
+            });
+          }
+        );
+      } catch (err) {
+        next(err);
+      }
+    }
+  ),
 ];
