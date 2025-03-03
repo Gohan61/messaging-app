970548c1f7f7870502e46b49ac05ef01df1c133c
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2025-03-03 18:09:36 +0100

    Doc: define form + error types

---
+++ messaging-socket-frontend/src/types/types.ts
@@ -0,0 +1,44 @@
+import { Dispatch, SetStateAction } from "react";
+
+export interface formState {
+  firstName: string;
+  lastName: string;
+  username: string;
+  password: string;
+  bio: string;
+}
+
+export type SubmitFunctionType = (
+  e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+  body: formState
+) => void;
+
+export interface singleValidationError {
+  type: string;
+  value: string;
+  msg: string;
+  path: string;
+  location: string;
+}
+
+export type validationErrorsType = singleValidationError[];
+
+export interface validationErrors {
+  firstName: string | null;
+  lastName: string | null;
+  username: string | null;
+  password: string | null;
+  bio: string | null;
+}
+
+export interface inputProps {
+  type: string;
+  id: string;
+  name: string;
+  value: string;
+  form: formState;
+  maxLength?: number;
+  stateSetter: Dispatch<SetStateAction<formState>>;
+  setValidationState: Dispatch<SetStateAction<validationErrors>>;
+  validationState: validationErrors;
+}
